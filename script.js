2b20b69ae68f8d75007a2145228411cc09e4e6c4
Luca-11 <luca.guillemot@gmail.com>
2024-01-30 15:27:13 +0100

    Add center alignment for the sun image

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ gsap.fromTo(
 );
 
 gsap.fromTo(
-  ".sun",
+  ".ceeeentre",
   {
     opacity: 0, // start from completely transparent
   },
@@ -27,7 +27,7 @@ gsap.fromTo(
 
     scrollTrigger: {
       trigger: "#test-2",
-      pin: ".sun",
+      pin: ".ceeeentre",
       start: "top 0%",
       end: "bottom 0%",
       // markers: true,
@@ -40,24 +40,62 @@ gsap.fromTo(
   ".sun",
   {
     scale: 1,
-    x: 0,
-    y: 0,
   },
   {
-    scale: 1.5,
-    x: 500,
-    y: 100,
+    scale: 0,
 
     scrollTrigger: {
       trigger: ".sun",
       pin: ".sun",
-      start: "top 50%",
+      start: "top 0%",
       end: "bottom 0%",
       markers: true,
-      scrub: 10,
+      scrub: 1,
     },
   }
 );
+
+// gsap.fromTo(
+//   ".sun",
+//   {
+//     opacity: 0, // start from completely transparent
+//   },
+//   {
+//     opacity: 1, // animate to completely visible
+
+//     scrollTrigger: {
+//       trigger: "#test-2",
+//       pin: ".sun",
+//       start: "top 0%",
+//       end: "bottom 0%",
+//       // markers: true,
+//       scrub: 1,
+//     },
+//   }
+// );
+
+// gsap.fromTo(
+//   ".sun",
+//   {
+//     scale: 1,
+//     x: 0,
+//     y: 0,
+//   },
+//   {
+//     scale: 1.5,
+//     x: 500,
+//     y: 100,
+
+//     scrollTrigger: {
+//       trigger: "#test-2",
+//       pin: ".sun",
+//       start: "top 25%",
+//       end: "bottom 0%",
+//       markers: true,
+//       scrub: 1,
+//     },
+//   }
+// );
 
 gsap.fromTo(
   "#test-3",
